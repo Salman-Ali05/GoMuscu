3a45360213154745744ba4f5d666846b3dd2e348
ianis-dev <ianischennaf@outlook.com>
2023-05-29 22:41:36 +0200

    New front

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,54 +45,84 @@ const Home = () => {
             <div className="Image"></div>
             <div className="container">
                 <h3>Dumbells :</h3>
-                {dumbells.map(dumbell => (
-                    <div key={dumbell._id}>
-                        <a href={'/client/dumbell/' + dumbell._id} className='item'><div className="container_item"><p>Name: {dumbell.name}</p>
-                            <p>Price: {dumbell.price}</p>
-                            <p>Weight: {dumbell.weight}</p>
+                <div class="container_dumbells">
+                    
+                    {dumbells.map(dumbell => (
+                        <div key={dumbell._id}>
+                            <a href={'/client/dumbell/' + dumbell._id}><div className="container_item"><p>Name: {dumbell.name}</p>
+                                <p>Price: {dumbell.price}</p>
+                                <p>Weight: {dumbell.weight}</p>
+                            </div>
+                            </a>
+
                         </div>
-                        </a>
 
-                    </div>
+                    ))
+                    }
+                </div>
 
-                ))
-                }
                 <h3>Benches :</h3>
-                {benches.map(bench => (
-                    <div key={bench._id}>
-                        <a href={'/client/bench/' + bench._id} className='item'><div className="container_item"><p>Name: {bench.name}</p>
-                            <p>Price: {bench.price}</p>
-                            <p>Features: {bench.features}</p>
-                        </div>
-                        </a>
+                <div class="container_benches">
+                    {benches.map(bench => (
+                        <div key={bench._id}>
+                            <a href={'/client/bench/' + bench._id}><div class="container_item container_item_benches"><p>Name: {bench.name}</p>
+                                <p>Price: {bench.price}</p>
+                                <p>Features: {bench.features}</p>
+                            </div>
+                            </a>
 
-                    </div>
-                ))
-                }
+                        </div>
+                    ))
+                    }
+                </div>
+
                 <h3>Rods :</h3>
-                {rods.map(rod => (
-                    <div key={rod._id}>
-                        <a href={'/client/rod/' + rod._id} className='item'><div className="container_item"><p>Name: {rod.name}</p>
-                            <p>Price: {rod.price}</p>
-                            <p>Weight: {rod.weight}</p>
-                        </div>
-                        </a>
+                <div class="container_rods">
+                    {rods.map(rod => (
+                        <div key={rod._id}>
+                            <a href={'/client/rod/' + rod._id}><div className="container_item container_item_rods"><p>Name: {rod.name}</p>
+                                <p>Price: {rod.price}</p>
+                                <p>Weight: {rod.weight}</p>
+                            </div>
+                            </a>
 
-                    </div>
-                ))
-                }
-                <h3>Plates :</h3>
-                {plates.map(plate => (
-                    <div key={plate._id}>
-                        <a href={'/client/plate/' + plate._id} className='item'><div className="container_item"><p>Name: {plate.name}</p>
-                            <p>Price: {plate.price}</p>
-                            <p>Weight: {plate.weight}</p>
                         </div>
-                        </a>
+                    ))
+                    }
 
-                    </div>
-                ))
-                }
+                </div>
+
+
+                {/* <h3>plate :</h3>
+                <div class="container_plate">
+                    {rods.map(rod => (
+                        <div key={rod._id}>
+                            <a href={'/client/plate/' + rod._id}><div className="container_item container_item_plate"><p>Name: {rod.name}</p>
+                                <p>Price: {rod.price}</p>
+                                <p>Weight: {rod.weight}</p>
+                            </div>
+                            </a>
+
+                        </div>
+                    ))
+                    }
+
+                </div> */}
+            
+                {/* <h3>Plates :</h3>
+                <div class="container_plate">
+                    {plates.map(plate => (
+                        <div key={plate._id}>
+                            <a href={'/client/plate/' + plate._id}><div className="container_item"><p>Name: {plate.name}</p>
+                                <p>Price: {plate.price}</p>
+                                <p>Weight: {plate.weight}</p>
+                            </div>
+                            </a>
+
+                        </div>
+                    ))
+                    }
+                </div> */}
             </div>
         </div >
 
